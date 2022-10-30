5c0b77dc5b9d7c71c14f062ec8cad4ab35712e59
Calvin <calvinbrownie@yahoo.com>
2022-10-30 07:26:06 -0500

    Initilized an Express application. Adding Environment Variables next.

---
+++ index.js
@@ -0,0 +1,7 @@
+// Importing Express
+const express = require('express');
+
+// Initilizing variable, 'app' via express
+const app = express();
+
+// Need to install a package to handle env variables //
